f72ad12b15261305ee42c9c8796e3fab1f429fac
itakuto <itakuto.pw4@gmail.com>
2019-06-12 11:23:58 +0900

    HelloWaseda

---
+++ HelloWaseda.java
@@ -0,0 +1,6 @@
+package info.hishiyama;
+public class HelloWaseda {
+	public static void main(String args[]){
+		System.out.println("12月15日\n石川拓人\n1X17C014\n\u5927,\u9688,\u8B1B,\u5802\n16進数文字コード 5927,9688,8B1B,5802:\u5927,\u9688,\u8B1B,\u5802");
+	}
+}
